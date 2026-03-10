dc8d3a0db8b75d5b01e54c93da88da3d91b43f29
Shayne Koestler <shayne.koestler+git@gmail.com>
2026-03-09 21:27:22 -0500

    adds aws cli and terraform binaries and basic Dockerfile

--- flake.nix
+++ flake.nix
@@ -6,13 +6,15 @@
     outputs = { self, nixpkgs }:
     let
         system = "aarch64-darwin"; # Adjust to "x86_64-linux" or "aarch64-linux" as needed
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in {
         devShells.${system}.default = pkgs.mkShell {
             packages = [
               pkgs.nodejs_25
               pkgs.nats-server
               pkgs.postgresql
+              pkgs.awscli2
+              pkgs.terraform
               pkgs.process-compose
               pkgs.httpie
             ];
